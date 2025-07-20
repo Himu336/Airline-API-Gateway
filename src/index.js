f25fe2836f167c28697f3c1699fa15fe431fd023
Himansh <himanshbansal200521@gmail.com>
2025-07-20 17:47:51 +0530

    Setup rate limiter and reverse proxy to microservices

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const { PORT } = require('./config');
+const { PORT, FLIGHTS_SERVICE_URL, BOOKING_SERVICE_URL } = require('./config');
 const apiRoutes = require('./routes');
-
+const ratelimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { create } = require('./services/user-service');
 const app = express();
+
+const limiter = ratelimit({
+    windowMs: 2 * 60 * 1000, // 15 minutes
+    max: 3, // Limit each IP to 100 requests per windowMs
+});
 
 app.use(express.json()); // For JSON bodies
 app.use(express.urlencoded({ extended: true })); // For form-urlencoded bodies
+
+app.use(limiter); // Apply rate limiting to all requests
+
+app.use('/flightsService', createProxyMiddleware({ target: FLIGHTS_SERVICE_URL, changeOrigin: true, pathRewrite: { '^/flightsService': '/' } }));
+app.use('/bookingService', createProxyMiddleware({ target: BOOKING_SERVICE_URL, changeOrigin: true, pathRewrite: { '^/bookingService': '/' } }));
 
 app.use('/api', apiRoutes);
 
